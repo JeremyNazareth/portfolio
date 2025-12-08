4f601a7b36229f8cf9aec4bb9a4947818939aa26
JeremyNazareth <jer.reyesr@outlook.com>
2025-12-08 19:16:48 -0300

    stye: add style text-weight more professional

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Page(){
             <h1 className="text-3xl">¡Bienvenido a mi portfolio!</h1>
             <div className="flex text-5xl items-end">
                 <p className="flex-initial leading-13 text-4xl text-white">Soy&nbsp;</p>
-                <p className="text-5xl text-white font-bold flex-initial sm:text-6xl">Jeremy Nazareth</p>
+                <p className="text-5xl text-white font-semibold flex-initial sm:text-6xl">Jeremy Nazareth</p>
             </div>
             <p className="text-3xl text-gray-300">front-end Developer</p>
-            <button className="secondary-text bg-secondary px-3 py-1 rounded-full my-4 ">Descargar CV</button>
+            <button className="border-secondary text-secondary bg-primary border-2 px-3 py-1 rounded-full my-4 shadow-primary text-lg">Descargar CV</button>
         </section>
         
         <div className="fixed flex items-center left-1/2 bottom-5">
@@ -26,7 +26,7 @@ export default function Page(){
             <h1 className="text-3xl font-bold text-white">Proyectos</h1>
             <article className="mt-2">
                 <div>
-                    <img className="border-2 rounded-3xl p-2 my-8 w-md shadow-primary" src="movieSearch_Project.png" alt="" />        
+                    <img className="border-2 rounded-3xl p-1 my-8 w-md shadow-primary" src="movieSearch_Project.png" alt="" />        
                     <h1 className="text-2xl font-bold"> Movie Search</h1>
                     <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis enim iure quod obcaecati magni voluptatem voluptates repellat libero aliquid commodi omnis porro, aspernatur soluta tempore beatae culpa perspiciatis! Placeat, minima?</p>
                 </div>
@@ -40,19 +40,20 @@ export default function Page(){
                         <p>Vite</p>
                     </li>
                     <li className="badge items-center">
-                        <img className="badgeIcon" src="vitejs.svg" alt="" />
-                        <p>Vite</p>
+                        <img className="badgeIcon" src="typescript.svg" alt="" />
+                        <p>Typescript</p>
                     </li>
                     <li className="badge items-center">
-                        <img className="badgeIcon" src="vitejs.svg" alt="" />
-                        <p>Vite</p>
+                        <img className="badgeIcon" src="html5.svg" alt="" />
+                        <p>Html 5</p>
                     </li>
                 </ul>
             </article>
             
             
         </section>
-
+        
+        
         <section id="skills">
             <h1 className="text-3xl font-bold text-white">Habilidades</h1>
             <h2 className="text-2xl w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime odit a natus vitae suscipit dolorem cum ducimus ut pariatur. Ipsam ut consectetur ab possimus modi deserunt neque eveniet! Provident, veniam!</h2>
