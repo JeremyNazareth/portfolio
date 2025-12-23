5131ae84e4a5417e6121d96ad2508be72e5113d0
JeremyNazareth <jer.reyesr@outlook.com>
2025-12-23 18:53:51 -0300

    style: add CopyEmailButtonAlert and CopyEmail animations

--- app/components/CopyEmailButton.tsx
+++ app/components/CopyEmailButton.tsx
@@ -2,22 +2,40 @@
 
 import { MdEmail } from "react-icons/md";
 import { IoCopySharp } from "react-icons/io5";
+import { useRef } from "react";
 
 export default function CopyEmailButton(){
 
+    
+    const alertContainerRef = useRef<HTMLDivElement>(null);
+    const copyEmailRef= useRef<HTMLDivElement>(null);
+
     function copyEmail(){
-        navigator.clipboard.writeText("jer.reyesr@outlook.com")
-        alert('Email copiado en el portapeles.')
+
+
+        if(alertContainerRef.current){
+            const alertContainerClass = 'copyEmailAlert-animation' 
+            const copyEmailButtonActive = 'copyEmail-container-active'
+            navigator.clipboard.writeText("jer.reyesr@outlook.com")
+            
+            copyEmailRef.current?.classList.add(copyEmailButtonActive)
+            alertContainerRef.current.classList.add(alertContainerClass)
+            
+            setTimeout(() => {alertContainerRef.current?.classList.remove(alertContainerClass)},6000)
+            setTimeout(() => {copyEmailRef.current?.classList.remove(copyEmailButtonActive)}, 300)
+        }
+        
     }
+
     return(
         <div>
-            <div className="copyEmailAlert-container fixed inset-x-0 flex justify-center -top-10">
+            <div className="fixed inset-x-0 flex justify-center -top-10" ref={alertContainerRef}>
                 <p className="bg-primary border-2 border-secondary rounded-full px-2 py-1">Email copiado en el portapeles.</p>
             </div>
-            <div onClick={() => copyEmail()} 
-            className='group justify-end flex transition-all cursor-pointer relative w-[27] h-[27] px-1  duration-600 overflow-hidden items-center
-            hover:justify-end hover:w-32 hover:bg-secondary hover:text-black
-            border border-2 border-secondary bg-primary rounded-full shadow-button'>
+            <div onClick={() => copyEmail()} ref={copyEmailRef}
+            className='copyEmail-container group justify-end flex transition-all cursor-pointer relative w-[27] h-[27] px-1  duration-600 overflow-hidden items-center
+            border border-2 border-secondary bg-primary rounded-full shadow-button
+            hover:justify-end hover:w-32 hover:bg-secondary hover:text-black hover:shadow-secondary'>
                 <p 
                 className=" transition-all w-24 absolute text-center right-6 duration-300 opacity-0 
                 group-hover:opacity-100">
